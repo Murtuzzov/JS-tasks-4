a992259c2f638a7c085e9f34de6aac4cdeffdbe1
murtuzzov <murtuzzov>
2024-02-21 21:52:22 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+//ЗАДАЧА No1
+// if (isNaN(numFirst) || isNaN(numSecond)) {
+//   alert("Пожалуйста, введите числовые значения для чисел.");
+// } else if (
+//   operator === "+" ||
+//   operator === "-" ||
+//   operator === "*" ||
+//   operator === "/"
+// ) {
+//   switch (operator) {
+//     case "+":
+//       result = parseInt(numFirst) + parseInt(numSecond);
+//       break;
+//     case "-":
+//       result = parseInt(numFirst) - parseInt(numSecond);
+//       break;
+//     case "*":
+//       result = parseInt(numFirst) * parseInt(numSecond);
+//       break;
+//     case "/":
+//       result = parseInt(numFirst) / parseInt(numSecond);
+//       break;
+//   }
+//   alert(`Результат: ${result}`);
+// } else {
+//   alert("Пожалуйста, введите допустимый арифметический оператор");
+// }
+
+//ЗАДАЧА No2
+// const fruits = prompt("Введите название фрукта:");
+
+// switch (fruits) {
+//   case "Яблоко":
+//     alert(`${fruits} есть в наличии`);
+//     break;
+//   case "Банан":
+//     alert(`${fruits} есть в наличии`);
+//     break;
+//   case "Гранат":
+//     alert(`${fruits} есть в наличии`);
+//     break;
+//   case "Груша":
+//     alert(`${fruits} есть в наличии`);
+//     break;
+//   case "Апельсин":
+//     alert(`${fruits} есть в наличии`);
+//     break;
+
+//   default:
+//     alert(`Увы, но ${fruits} нет в наличии`);
+//     break;
+// }
